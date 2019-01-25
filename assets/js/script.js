249772103113044513fc84e327e13e7987a8ebd5
Amadewe <dewevre.amandine@gmail.com>
2019-01-25 10:07:45 +0100

    Ajout des commentaires et 3 px au script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+// mon evenement: onMousseOver
+// J'appelle ma fonction "changeborder"
 function changeborder() {
-document.getElementById('image1').style.border = 'solid red';
+  // je veux mettre mon bord d'image à 3 px et rouge du coup je mets la propriété : .style.border
+document.getElementById('image1').style.border = '3px solid red';
 }
+// mon evenement: onMousseOut
+// J'appelle ma fonction "initialborder"
 function initialborder() {
+// avec ma propriété je résinialiser mon bord : initial 
 document.getElementById('image1').style.border = 'initial';
 }
