5b4ef2baafb819fc712fac31551175cb904d7f09
Amadewe <dewevre.amandine@gmail.com>
2019-01-31 09:23:13 +0100

    Correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-// mon evenement: onMousseOver
-// J'appelle ma fonction "changeborder"
-function changeborder() {
-  // je veux mettre mon bord d'image à 3 px et rouge du coup je mets la propriété : .style.border
+// mon evenement: onmousseover
+// J'appelle ma fonction "changeBorder"
+function changeBorder() {
+  // je veux mettre mon bord d'image à 3 px et en rouge du coup je mets la propriété : .style.border
 document.getElementById('image1').style.border = '3px solid red';
 }
-// mon evenement: onMousseOut
-// J'appelle ma fonction "initialborder"
-function initialborder() {
-// avec ma propriété je résinialiser mon bord : initial 
+// mon evenement: onmousseout
+// J'appelle ma fonction "initialBorder"
+function initialBorder() {
+// avec ma propriété je réinitialise mon bord : initial
 document.getElementById('image1').style.border = 'initial';
 }
